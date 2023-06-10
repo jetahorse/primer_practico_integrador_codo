2cf5ff4321cadb24a0754c43531b448416041898
jetahorse <federicoaugustotschopp@MacBook-Pro-de-Federico.local>
2023-06-10 13:17:12 -0300

    commit con script de cálculo total a pagar funcionando

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function calcTotalaPagar(descuento,cantidad){
-    return (200*cantidad*descuento);
+    return ((200*cantidad)*(1-descuento/100));
 }
 
-var nombre=document.querySelector("inputNombre");
-var apellido=document.querySelector("inputAepllido");
-var email=document.querySelector("inputMail");
-var cantidad=document.querySelector("inputCantidad");
-var categoria=document.querySelector("inputCategoria");
-var importePagoTotal=document.querySelector("importeTotal");
-var validNombre=document.querySelector("error-nombre");
-var validApellido=document.querySelector("error-apellido");
-var validEmail=document.querySelector("error-email");
-var validCantidad=document.querySelector("error-cantidad");
 
-let frmCompra=document.querySelector("formCompra");
+let frmCompra=document.querySelector("#formCompra");
 frmCompra.addEventListener("submit", function(event){
+    event.preventDefault();
+    var nombre=document.querySelector("#inputNombre");
+    var apellido=document.querySelector("#inputApellido");
+    var email=document.querySelector("#inputMail");
+    var cantidad=document.querySelector("#inputCantidad");
+    var categoria=document.querySelector("#inputCategoria");
+    var importePagoTotal=document.querySelector("#importeTotal");
+    var validNombre=document.querySelector("#error-nombre");
+    var validApellido=document.querySelector("#error-apellido");
+    var validEmail=document.querySelector("#error-email");
+    var validCantidad=document.querySelector("#error-cantidad");
+
+    
     let expresionRemail=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     let valido=true;         
@@ -32,29 +35,34 @@ frmCompra.addEventListener("submit", function(event){
         valido=false;
     }
 
-    if (isNaN(cantidad.value)){
+    if (isNaN(cantidad.value) && (!Number.isInteger(cantidad.value)) ){
         validCantidad.textContent="Debe escribir un número entero";
         valido=false;
     }
+
+    if (cantidad.value===""){
+        validCantidad.textContent="Indique una cantidad";
+    }
+
     let totalPago;
     if (valido==true){
 
         switch (categoria.value){
             case "Estudiante":
-                totalPago=calcTotalaPagar(0.80,parseInt(cantidad.value));
+                totalPago=calcTotalaPagar(80,parseInt(cantidad.value));
                 break;
             case "Trainee":
-                totalPago=calcTotalaPagar(0.50,parseInt(cantidad.value));
+                totalPago=calcTotalaPagar(50,parseInt(cantidad.value));
                 break;
             case "Junior":
-                totalPago=calcTotalaPagar(0.15,parseInt(cantidad.value));
+                totalPago=calcTotalaPagar(15,parseInt(cantidad.value));
                 break;
             default:
-                totalPago=calcTotalaPagar(1,parseInt(cantidad.value));
+                totalPago=calcTotalaPagar(100,parseInt(cantidad.value));
                 break;
 
         }
     }
-    importePagoTotal.textContent=totalPago;
-    event.preventDefault();    
+    importePagoTotal.innerHTML="Total a pagar: $ "+totalPago;
+        
 })
